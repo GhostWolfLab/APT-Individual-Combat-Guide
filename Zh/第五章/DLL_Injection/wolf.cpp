cef5e14c9c006ef4bb3d92064ba6c5c7b165612a
GhostWolfLab <snowwolfmysql@gmail.com>
2024-08-16 17:05:52 +0800

    Create wolf.cpp

---
+++ Zh/第五章/DLL_Injection/wolf.cpp
@@ -0,0 +1,22 @@
+#include <windows.h>
+#pragma comment (lib, "user32.lib")
+
+BOOL APIENTRY DllMain(HMODULE hModule,  DWORD  nReason, LPVOID lpReserved) {
+  switch (nReason) {
+  case DLL_PROCESS_ATTACH:
+    MessageBox(
+      NULL,
+      "Hi,I am Snow Wolf.",
+      "Ghost Wolf Lab",
+      MB_OK
+    );
+    break;
+  case DLL_PROCESS_DETACH:
+    break;
+  case DLL_THREAD_ATTACH:
+    break;
+  case DLL_THREAD_DETACH:
+    break;
+  }
+  return TRUE;
+}
